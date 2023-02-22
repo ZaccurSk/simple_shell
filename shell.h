fe7cc9a7ee5b1afb0f648087a50490909c4c1797
Skhumbuzo Zaccur Mahlangu <SkhumbuzoRakgoale@outlook.com>
2023-02-22 23:31:23 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,112 +1,47 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
-
-/**###### environ var ######*/
-
-extern char **environ;
-
-/**##### MACROS ######*/
-
-#define BUFSIZE 1024
-#define DELIM " \t\r\n\a"
-#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
-
-/**###### LIBS USED ######*/
+#define _GNU_SOURCE
 
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <stdlib.h>
-#include <signal.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <linux/limits.h>
-
-
-
-
-
-/**###### STRING FUNCTION ######*/
-
-char *_strtok(char *str, const char *tok);
-unsigned int check_delim(char c, const char *str);
-char *_strncpy(char *dest, char *src, int n);
-int _strlen(char *s);
-int _putchar(char c);
-int _atoi(char *s);
-void _puts(char *str);
-int _strcmp(char *s1, char *s2);
-int _isalpha(int c);
-void array_rev(char *arr, int len);
-int intlen(int num);
-char *_itoa(unsigned int n);
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, char *src);
-char *_strchr(char *s, char c);
-int _strncmp(const char *s1, const char *s2, size_t n);
-char *_strdup(char *str);
-
-/**###### MEMORIE  MANGMENT ####*/
-
-void free_env(char **env);
-void *fill_an_array(void *a, int el, unsigned int len);
-char *_memcpy(char *dest, char *src, unsigned int n);
-void *_calloc(unsigned int size);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void free_all(char **input, char *line);
-
-/**###### INPUT Function ######*/
-
-void prompt(void);
-void signal_to_handel(int sig);
-char *_getline(void);
-
-/** ###### Command parser and extractor ###*/
-
-int path_cmd(char **line);
-char *_getenv(char *name);
-char **parse_cmd(char *cmd);
-int handle_builtin(char **cmd, int er);
-void read_file(char *filename, char **argv);
-char *build(char *token, char *value);
-int check_builtin(char **cmd);
-void creat_envi(char **envi);
-int check_cmd(char **tokens, char *line, int count, char **argv);
-void treat_file(char *line, int counter, FILE *fd, char **argv);
-void exit_bul_for_file(char **cmd, char *line, FILE *fd);
-
-/** ####BUL FUNC #####*/
-
-void hashtag_handle(char *buff);
-int history(char *input);
-int history_dis(char **cmd, int er);
-int dis_env(char **cmd, int er);
-int change_dir(char **cmd, int er);
-int display_help(char **cmd, int er);
-int echo_bul(char **cmd, int er);
-void  exit_bul(char **cmd, char *input, char **argv, int c);
-int print_echo(char **cmd);
-
-/** ####error handle and Printer ####*/
-void print_number(unsigned int n);
-void print_number_in(int n);
-void print_error(char *line, int c, char **argv);
-void _prerror(char **argv, int c, char **cmd);
-
-
+#include <sys/types.h>
+#include <signal.h>
 /**
- * struct bulltin - contain bultin to handle and function to excute
- * @command:pointer to char
- * @fun:fun to excute when bultin true
+ * struct environ_list - singly linked list
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-
-typedef struct  bulltin
+typedef struct environ_list
 {
-	char *command;
-	int (*fun)(char **line, int er);
-} bul_t;
+	char *str;
+	struct environ_list *next;
+} env_list;
 
-#endif
+void sighand(int signum);
+extern char **environ;
+void print_env(void);
+void error(char *str, int line_counter, int flag);
+int _strlen(char *s);
+void rev_string(char *s);
+char *_itoa(int n);
+unsigned int get_pid(void);
+unsigned int get_ppid(void);
+char *stringdup(const char *str);
+int toksize(char *str);
+char **tokenize(char *str);
+int few(pid_t c_pid, char **arr, int line_counter);
+int str_comp(char **arr, int size);
+char *_getenv(const char *name);
+int _which(char *file);
+char *str_concat(char *s1, char *s2, int flag);
+int path(char **arr, int line_counter);
+
+
+#endif /* _SHELL_H_ */
