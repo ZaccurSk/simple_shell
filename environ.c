4f6af8fa751837ecec2d90013e2bd8f2c449dc8c
Skhumbuzo Zaccur Mahlangu <SkhumbuzoRakgoale@outlook.com>
2023-02-22 23:23:24 +0200

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,92 +1,67 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * print_env - a function that prints the environment.
+ * Return: 0.
  */
-int _myenv(info_t *info)
-{
-	print_list_str(info->env);
-	return (0);
-}
 
-/**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
- */
-char *_getenv(info_t *info, const char *name)
+void print_env(void)
 {
-	list_t *node = info->env;
-	char *p;
+	int i = 0;
 
-	while (node)
+	while (environ[i])
 	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
-		node = node->next;
+		/* prints in form of "variable=value" */
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		i++;
+		write(STDOUT_FILENO, "\n", 1);
+	}
+}
+/**
+ * _getenv - a function that gets an environment variable.
+ * (without using getenv)
+ * @name: the variable to be compared with environ.
+ * Return: char.
+ */
+char *_getenv(const char *name)
+{
+	int i, j, length = 0;
+
+	while (name[length] != '\0')
+		length++;
+
+	if (length == 0)
+		return (NULL);
+
+	for (i = 0 ; environ[i] != NULL ; i++)
+	{
+		for (j = 0 ; name[j] != '\0' ; j++)
+		{
+			if (environ[i][j] != name[j])
+				break;
+		}
+		if (name[j] == '\0')
+			return (environ[i] + j + 1);
 	}
 	return (NULL);
 }
-
 /**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _which - This function checks whether a file exists in PATH
+ * @file: The file to check
+ * Return: On success, 0. Otherwise, -1.
  */
-int _mysetenv(info_t *info)
+
+int _which(char *file)
 {
-	if (info->argc != 3)
+	struct stat status;
+
+	while (file)
 	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
+		if (stat(file, &status) == 0)
+		{
+			return (0);
+		}
+		break;
 	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
-}
-
-/**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
-int _myunsetenv(info_t *info)
-{
-	int i;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
-}
-
-/**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
+	return (-1);
 }
