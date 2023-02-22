55a3d87a16039b9515a968d83c0dda3544c15174
Skhumbuzo Zaccur Mahlangu <SkhumbuzoRakgoale@outlook.com>
2023-02-22 23:24:15 +0200

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,85 +1,114 @@
 #include "shell.h"
-
 /**
- *_eputs - prints an input string
- * @str: the string to be printed
- *
- * Return: Nothing
+ * _itoa - converts int to string
+ * @n: integer given
+ * Return: new string
  */
-void _eputs(char *str)
-{
-	int i = 0;
 
-	if (!str)
-		return;
-	while (str[i] != '\0')
+char *_itoa(int n)
+{
+
+	long x;
+	int count, adjust;
+	char *string;
+
+	/* creating space for max/min int character string */
+	string = malloc(sizeof(char) * 12);
+	/* if string passed is NULL, return NULL */
+	if (string == NULL)
+		return (NULL);
+
+	/* cast the passed integer n into a long variable */
+	x = n;
+
+	/* initialize count to zero */
+	count = 0;
+
+	/* if x is a negative number */
+	if (x < 0)
 	{
-		_eputchar(str[i]);
-		i++;
+		/* append the negative sign */
+		string[count++] = '-';
+		/* convert the number to a positive number */
+		x = x * -1;
 	}
+
+	/* if x is greater than 9 */
+	if (x > 9)
+	{
+		adjust = 10;
+
+		/* if x is > 10, parse through to get to highest place */
+		while (x / adjust >= 10)
+			adjust = adjust * 10;
+
+		/* append the converted highest's place int into the string */
+		string[count++] = ((x / adjust) + '0');
+
+		/* if x is greater than or equal to 10 */
+		while (adjust >= 10)
+		{
+			/* move backward through number */
+			adjust = adjust / 10;
+			/* append converted numbers into the string */
+			string[count++] = (((x / adjust) % 10)  + '0');
+		}
+
+	}
+	/* else if x is a single digit, convert and append to string */
+	else
+		string[count++] = x + '0';
+
+	/* append null */
+	string[count] = 0;
+	return (string);
 }
-
 /**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * error - This function returns custom error messages
+ * @str: The argument at index 0
+ * @line: Line count from getline function
+ * @flag: To determine which error message to output
  */
-int _eputchar(char c)
-{
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+void error(char *str, int line, int flag)
+{
+	char *line_counter = NULL;
+	char *shell = _getenv("_");
+
+	line_counter = _itoa(line);
+	write(STDERR_FILENO, shell, _strlen(shell));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, line_counter, _strlen(line_counter));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, str, _strlen(str));
+
+	if (flag == 0)
+		write(STDERR_FILENO, ": not found\n", 13);
+	else if (flag == 1)
+		perror(" ");
+
+	free(line_counter);
 }
-
 /**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * get_pid - function to get process ID of the calling process
+ * Return: pid;
  */
-int _putfd(char c, int fd)
+unsigned int get_pid(void)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+	unsigned int pid;
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(fd, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+	pid = getpid();
+	return (pid);
 }
-
 /**
- *_putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
+ * get_ppid - function to return the process ID of the
+ * parent process of the calling process
+ * Return: ppid
  */
-int _putsfd(char *str, int fd)
+unsigned int get_ppid(void)
 {
-	int i = 0;
+	unsigned int ppid;
 
-	if (!str)
-		return (0);
-	while (*str)
-	{
-		i += _putfd(*str++, fd);
-	}
-	return (i);
+	ppid = getppid();
+	return (ppid);
 }
